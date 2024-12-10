5a93da40c77a2d89de10b5c4e0db22a740cbcd3d
Gabriel Falk <113801587+falkones04@users.noreply.github.com>
2024-12-10 12:36:29 -0300

    tratativa inteiros

--- Rouba_Monte/Program.cs
+++ Rouba_Monte/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Reflection.Metadata;
 
-
 namespace Rouba_Monte
 {
     class Program
@@ -16,29 +15,48 @@ namespace Rouba_Monte
             Console.WriteLine("3 - Sair");
             Console.WriteLine("==============================");
         }
+        static int LerNumero()
+        {
+            int numero;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out numero)) 
+                {
+                    return numero; 
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, insira um número válido.");
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             Jogo mesa1 = null;
             Fila jogador = null;
             int op = -1;
+
             while (op != 3)
             {
                 Painel();
-                op = int.Parse(Console.ReadLine());
+                op = LerNumero(); 
+
                 switch (op)
                 {
                     case 1:
-                    while(true){
-                        if (jogador == null)
+                        while (true)
                         {
-                            Console.WriteLine("Insira o numero de cartas");
-                            int numCartas = int.Parse(Console.ReadLine());
-                            Console.WriteLine("Insira o numero de jogadores");
-                            int numJogador = int.Parse(Console.ReadLine());
-                            mesa1 = new Jogo(numCartas, numJogador);
-                            jogador = mesa1.ColocarPlayer(numJogador);
-                            mesa1.RealizarPartida();
-                            Console.WriteLine("Deseja jogar novamente? S ou N");
+                            if (jogador == null)
+                            {
+                                Console.WriteLine("Insira o numero de cartas");
+                                int numCartas = LerNumero(); 
+                                Console.WriteLine("Insira o numero de jogadores");
+                                int numJogador = LerNumero(); 
+                                mesa1 = new Jogo(numCartas, numJogador);
+                                jogador = mesa1.ColocarPlayer(numJogador);
+                                mesa1.RealizarPartida();
+                                Console.WriteLine("Deseja jogar novamente? S ou N");
                                 if (Console.ReadLine()?.ToUpper().Trim() == "S")
                                 {
                                     continue;
@@ -48,34 +66,35 @@ namespace Rouba_Monte
                                     break;
                                 }
                             }
-                        else{
-                            Console.WriteLine("Insira o numero de cartas");
-                            int numCartas = int.Parse(Console.ReadLine());
-                            mesa1 = new Jogo(numCartas, jogador);
-                            mesa1.RealizarPartida();
-                            Console.WriteLine("Deseja jogar novamente? S ou N");
-                                if(Console.ReadLine()?.ToUpper().Trim() == "S")
-                                { 
+                            else
+                            {
+                                Console.WriteLine("Insira o numero de cartas");
+                                int numCartas = LerNumero(); 
+                                mesa1 = new Jogo(numCartas, jogador);
+                                mesa1.RealizarPartida();
+                                Console.WriteLine("Deseja jogar novamente? S ou N");
+                                if (Console.ReadLine()?.ToUpper().Trim() == "S")
+                                {
                                     continue;
                                 }
                                 else
                                 {
                                     break;
                                 }
-                            break;
+                            }
                         }
-                    }
                         break;
                     case 2:
                         string nome = Console.ReadLine();
                         foreach (var jogador1 in mesa1.jogadores)
                         {
-                            if(jogador1.Nome==nome)
+                            if (jogador1.Nome == nome)
                             {
                                 Console.Write($"Ranking {nome}:\n");
                                 int[] temp = jogador1.ranking.ToArray();
-                                for(int i =0; i<temp.Length;i++){
-                                     Console.Write($"{temp[i]} ");
+                                for (int i = 0; i < temp.Length; i++)
+                                {
+                                    Console.Write($"{temp[i]} ");
                                 }
                                 Console.Write("\n");
                             }
